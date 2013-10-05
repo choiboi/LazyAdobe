f0addd15985efbaef4b45c4d81428097a5651528
choiboi <james.choiboi@gmail.com>
2013-10-05 09:35:26 -0400

    Modified js file to save sample js code as jsx.

--- js/index.js
+++ js/index.js
@@ -1,48 +1,6 @@
 // Start here.
 
-window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
-
-var onInitFs = function(fs) {
-
-  	fs.root.getFile('log.txt', {create: true, exclusive: false}, function(fileEntry) {
-	    fileEntry.createWriter(function(fileWriter) {
-	      	var blob = new Blob(['Lorem Ipsum'], {type: 'text/plain'});
-
-	      	fileWriter.write(blob);
-  		});
-  	}, errorHandler);
-};
-
 $('#writeToFile').on('click', function() {
-	// window.requestFileSystem(window.TEMPORARY, 1024*1024, onInitFs, errorHandler);
-	var blob = new Blob(['Lorem Ipsum'], {type: 'text/plain'});
-saveAs(blob, "document.txt");
+	var blob = new Blob(["if (documents.length == 0) { alert('There are no documents open.'); var flowerDoc = open(File('~/Desktop/test/test_image.png'))}"], {type: 'text/plain'});
+	saveAs(blob, "document.jsx");
 });
-
-var errorHandler = function(e) {
-	var msg = '';
-
-  switch (e.code) {
-    case FileError.QUOTA_EXCEEDED_ERR:
-      msg = 'QUOTA_EXCEEDED_ERR';
-      break;
-    case FileError.NOT_FOUND_ERR:
-      msg = 'NOT_FOUND_ERR';
-      break;
-    case FileError.SECURITY_ERR:
-      msg = 'SECURITY_ERR';
-      break;
-    case FileError.INVALID_MODIFICATION_ERR:
-      msg = 'INVALID_MODIFICATION_ERR';
-      break;
-    case FileError.INVALID_STATE_ERR:
-      msg = 'INVALID_STATE_ERR';
-      break;
-    default:
-      msg = 'Unknown Error';
-      break;
-  };
-
-  	alert('Error: ' + msg);
-
-};
